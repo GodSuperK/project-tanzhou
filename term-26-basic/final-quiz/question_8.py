6f2022fffd51ecb41e03084f3b5c182cec8c329b
ABU <iamliuche@163.com>
2018-07-06 11:44:56 +0800

    delete exchange_body()

--- term-26-basic/final-quiz/question_8.py
+++ term-26-basic/final-quiz/question_8.py
@@ -2,24 +2,6 @@
 filename = question_8.py
 author = 孔德成
 """
-
-def exchange_body(d, iter2):
-    if len(d) == len(iter2):
-        print('参数长度不符合要求')
-        return
-
-    if hasattr(d, 'keys') and not hasattr(iter2, 'keys'):
-        if len(d) < len(iter2):
-            result_d = dict(zip(d, iter2))
-            result_t = tuple(d.values())
-        # TODO(len(d)> len(iter2))
-        return result_d, result_t
-    else:
-        print('arg1 is not dict-like object, or arg2不是除类字典对象以外的可迭代对象')
-        return
-
-
-
 def exchange_body_1(d, l):
     if len(d) == len(l):
         print('参数长度不符合要求')
