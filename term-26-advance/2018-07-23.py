1d3d003aff9a9fbc8615506e63a40b47c8066f7e
ABU <iamliuche@163.com>
2018-07-24 17:45:42 +0800

    update 2018-07-23.py

--- term-26-advance/2018-07-23.py
+++ term-26-advance/2018-07-23.py
@@ -1,24 +1,54 @@
 import logging
 
+
 class Logger(object):
-    """日至记录器"""
+    """日志记录器"""
     fmt = ("%(asctime)s %(filename)s[line:%(lineno)d]"
            "%(levelname)s - %(message)s")
-    datefmt = "%m/%d/%Y %I:%M:%S"
+
+    fmt2 = ("\033[0;34m%(asctime)s\033[0m"
+            " \033[0;31m%(levelname)s\033[0m"
+            " [\033[0;32mline:%(lineno)d\033[0m %(message)s]")
+
+    datefmt = "%Y-%m-%d %I:%M:%S"
+
+    formatter = None
 
     @classmethod
-    def add_handler(cls, handler):
+    def add_handler(cls, logger, handler):
+        """将Handler 实例加入到 Logger中"""
         handler.setLevel(logging.WARNING)
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+        handler.setFormatter(cls.formatter)
+        logger.addHandler(handler)
 
-    @staticmethod 
-    def config():
-        logger = logging.getLogger("{}_log".format(__name__))
+    @classmethod
+    def get_logger(cls):
+        """返回一个日志记录器"""
+        logger = logging.getLogger(__name__)
         logger.setLevel(logging.INFO)
-        formatter = logging.Formatter(fmt=fmt, datefmt=datefmt)
-        return logger 
+        cls.formatter = logging.Formatter(fmt=cls.fmt2, datefmt=cls.datefmt)
+        return logger
+
+
+def logger_init():
+    logger = Logger.get_logger()
+
+    console_handler = logging.StreamHandler()
+    file_logging_handler = logging.FileHandler("program.log")
+
+    Logger.add_handler(logger, console_handler)
+    Logger.add_handler(logger, file_logging_handler)
+
+    return logger
+
+
+def main():
+    logger = logger_init()
+    try:
+        import xxx
+    except Exception as e:
+        logger.error(e)
 
 
 if __name__ == "__main__":
-    logger = Logger()
+    main()
