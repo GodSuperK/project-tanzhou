cf636e37c02feaf3c70c5d6790c33220140fdd79
ABU <iamliuche@163.com>
2018-07-31 10:51:00 +0800

    Add 2018-07-31

---
+++ term-26-advance/2018-07-31/client.py
@@ -0,0 +1,23 @@
+import socket
+
+client = socket.socket()
+try:
+    client.connect(('127.0.0.1', 9001))
+except ConnectionRefusedError as e:
+    print(e)
+else:
+    print("The connection is successful")
+
+while True:
+    message = input("> ")
+    if not message:
+        continue
+
+    if message == 'q':
+        break
+
+    client.send(message.encode("utf8"))
+    recv_info = client.recv(1024)
+    print('SERVER 说: ', recv_info.decode("utf8"))
+
+client.close()
---
+++ term-26-advance/2018-07-31/server-1.0.py
@@ -0,0 +1,68 @@
+import socket
+
+# 非阻塞套接字实现并发服务器
+"""
+测试结果
+1. 多客户端连接, 不排队 (done)
+2. 多客户端消息处理, 不排队伍 (done)
+3. 断开连接, 没有问题
+4. 
+"""
+
+
+class MyTCPServer(object):
+
+    def __init__(self, address, num=5):
+        self.server = socket.socket()
+        self.server.bind(address)
+        self.server.listen(num)
+        self.server.setblocking(False)
+        self.connection_pool = []
+        self.address_pool = []
+
+    def _session_loop(self):
+        # session 轮询
+        for conn in self.connection_pool:
+            i = self.connection_pool.index(conn)
+            ip = self.address_pool[i]
+            try:
+                data = conn.recv(1024)
+
+                if not data:
+                    conn.close()
+                    self.connection_pool.remove(conn)
+                    self.address_pool.remove(ip)
+                    print("客户端 {} 已断开连接".format(ip))
+
+                else:
+                    print("{} SEND: {}".format(ip, data.decode('utf8')))
+                    conn.send(data)
+            except BlockingIOError:
+                pass
+
+    def start(self):
+        # 服务器启动提示消息
+        print("Successfully launched")
+
+        # 轮询
+        while True:
+            # connect 轮询
+            try:
+                conn, addr = self.server.accept()
+                print("{} 已连接".format(addr[0]))
+                conn.setblocking(False)
+                self.connection_pool.append(conn)
+                self.address_pool.append(addr[0])
+            except BlockingIOError as e:
+                pass
+            # session 轮询
+            self._session_loop()
+
+
+def main():
+    server = MyTCPServer(('127.0.0.1', 9001))
+    server.start()
+
+
+if __name__ == "__main__":
+    main()
