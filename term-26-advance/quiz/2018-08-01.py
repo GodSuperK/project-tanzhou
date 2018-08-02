3ed1e8abd1791e9cb1ba10154fc32e6ab1fe2446
Detson <iamliuche@163.com>
2018-08-02 11:37:09 +0800

    上传8月1日的作业

--- term-26-advance/quiz/2018-08-01.py
+++ term-26-advance/quiz/2018-08-01.py
@@ -1,15 +1,31 @@
+import threading
 import multiprocessing
 import socket
 
 """
 用面向对象的方式实现并发服务器
-
-主进程管理客户端的连接
-新建进程管理服务器和客户端的通信
 """
 
 
-class CommunicationProcess(multiprocessing.Process):
+class MyProcess(multiprocessing.Process):
+
+    def __init__(self, conn, addr):
+        super().__init__()
+        self.conn = conn
+        self.addr = addr
+
+    def run(self):
+        while True:
+            data = self.conn.recv(1024)
+            if not data:
+                print('{}已经断开连接'.format(self.addr))
+                self.conn.close()
+                break
+            print(data.decode('utf8'))
+            self.conn.send(data)
+
+
+class MyThread(threading.Thread):
 
     def __init__(self, conn, addr):
         super().__init__()
@@ -40,7 +56,9 @@ def main():
         conn, addr = server.accept()
         print("{}已经连接".format(addr))
 
-        p = CommunicationProcess(conn, addr)
+        # 多线程并发服务器
+        p = MyThread(conn, addr)
+        # p = MyProcess(conn, addr)
         p.start()
 
 
